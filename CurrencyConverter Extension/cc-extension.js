23c4548f68946a43020db69a7e7fcdffd3aa6847
Killercat <rayershih@gmail.com>
2019-12-15 09:48:32 +0800

    Remove unnecessery JS log and callbacks

--- CurrencyConverter Extension/cc-extension.js
+++ CurrencyConverter Extension/cc-extension.js
@@ -3,14 +3,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 document.addEventListener("contextmenu", function (event) {
-  console.log(event)
-  if (event.srcElement && isImgUrl(event.srcElement.src)) {
-    //console.log(event.srcElement.src)
-    //We can start handle picture...
-    //alert(event.srcElement.src)
-    safari.extension.setContextMenuEventUserInfo(event, { "imageSrc": event.srcElement.src });
-  }
-
   let selected = window.getSelection().toString();
   if(selected) {
     safari.extension.setContextMenuEventUserInfo(event, {"selected": selected})
@@ -23,27 +15,4 @@ function messageHandler(message) {
     if(message.name === "CurrencyExchangeResult") {
         console.log(message)
     }
-}
-
-
-
-function isImgUrl(url) {
-  if(!isValidUrl(url)) {
-    return false;
-  }
-  let pure = getPathFromUrl(url)
-  return (pure.match(/\.(jpeg|jpg|gif|png)$/) != null);
-}
-
-function getPathFromUrl(url) {
-  return url.split("?")[0];
-}
-
-const isValidUrl = (string) => {
-  try {
-    new URL(string);
-    return true;
-  } catch (_) {
-    return false;
-  }
 }
